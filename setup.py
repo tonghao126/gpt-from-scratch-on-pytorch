dba642fc08eda228fc983549299cfd6888e48f4e
tonghao126 <tonghao126@gmail.com>
2024-03-19 11:09:54 -0400

    First commit

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Following Andrej Karpathy's video on building GPT from scratch, main goal is to learn PyTorch',
+    description="Following Andrej Karpathy's video on building GPT from scratch, main goal is to learn PyTorch",
     author='Hao',
     license='',
 )
+
